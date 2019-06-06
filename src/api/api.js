da1572b6da52ca0f51eec86c7376913bc01962b1
Alex X Xu <xualexander1@gmail.com>
2019-06-05 23:46:40 -0700

    api changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,24 +1,24 @@
-const API_URL = "http://localhost:3001";
+const API_URL = 'http://localhost:5000/api';
 
 // Takes a picture File object and sends a post
-const upload = async picture => {
-  try {
-    console.log(picture);
-    // const fd = new FormData();
-    // fd.append("upl", localStorage.myfile, "blobby.txt");
-    const res = await fetch(API_URL + "/upload", {
-      method: "POST",
-      body: JSON.stringify(picture)
-    });
-    return {
-      err: null,
-      data: res
-    };
-  } catch (err) {
-    return {
-      err
-    };
-  }
+const upload = async (picture) => {
+	try {
+		console.log(picture);
+		const fd = new FormData();
+		fd.append('carrier', picture, 'to_encode.blob');
+		const res = await fetch(API_URL + '/image_encode', {
+			method: 'POST',
+			body: fd
+		});
+		return {
+			err: null,
+			data: res
+		};
+	} catch (err) {
+		return {
+			err
+		};
+	}
 };
 
 export { upload };
